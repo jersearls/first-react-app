37b9e1535bd41a796316b6d6cd12d2fd993b4883
Jeremy Searls <searls@me.com>
2017-05-25 10:53:36 -0400

    state and stateless componenents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+//class component can have 'state'
 class App extends React.Component {
   render(){
     return <h1>Hello World</h1>
   }
 }
+
+//stateless function, simple return
+//const App = () => <h1>Hello stateless</h1>
 
 export default App
 
