48218019ba45c3e310166ae7c0e8bb3985fcfe13
Jeremy Searls <searls@me.com>
2017-05-25 11:29:33 -0400

    add additional elements to class component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import React from 'react';
 //class component can have 'state'
 class App extends React.Component {
   render(){
-    return <h1>Hello World</h1>
+    return (
+      <div>
+      <h1>Hello World</h1> 
+      <b>Bold</b>
+      </div>
+    )
   }
 }
 
