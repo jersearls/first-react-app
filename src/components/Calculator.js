e9b64df0181403976204809a16a0ed93f69e876b
Jeremy Searls <searls@me.com>
2017-06-14 11:26:19 -0400

    create style.css, add page styles

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,20 +4,21 @@ import { render } from 'react-dom';
 class Calculator extends React.Component {
   render() {
     return(
-      <form className="enterInfo">
-        <h1>Enter your current imperial height and weight.</h1>
-        <h2>Enter Height</h2>
-        <heightField>
-            {/* add padding to text inputs in style sheet */}
-            <input type="text" name="heightFeet" required placeholder="Enter Feet"/>
-            <input type="text" name="heightInches" required placeholder="Enter Inches"/>
+      <form>
+        <h1>Enter Your Current Imperial Height and Weight</h1>
+        <info className="body">
+          <h2>Enter Your Height</h2>
+          <heightField>
+              <input type="text" className="field" name="heightFeet" required placeholder="Enter Feet"/>
+              <input type="text" className="field" name="heightInches" required placeholder="Enter Inches"/>
           </heightField>             
-        <br />
-        <h2>Enter Weight</h2>
-        <input type="text" name="weight" required placeholder="Enter Lbs"/>
-        <br />
-        <br />
-        <button type="submit" id="calcBtn">Calculate BMI</button>
+          <br />
+          <h2>Enter Your Weight</h2>
+          <input type="text" className="field" name="weight" required placeholder="Enter Lbs"/>
+          <br />
+          <br />
+          <button type="submit" className="submit" id="calcBtn">Calculate BMI -></button>
+        </info>
       </form>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Calculator from './components/Calculator'
-//import './css/style.css';
+import './css/style.css';
 
 render(<Calculator/>, document.querySelector('#main'));
